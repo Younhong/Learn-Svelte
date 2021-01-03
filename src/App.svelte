05e54f35fe448cae841ba01967637f94b6516369
younhong <younhong@kakao.com>
2021-01-03 16:28:49 +0900

    finish lecture

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
+	import AddPlayer from './AddPlayer.svelte';
 
 	let players = [
 		{
@@ -16,15 +17,27 @@
 			points: 87
 		},
 	];
+
+	const addPlayer = (e) => {
+		const newPlayer = e.detail;
+		players = [...players, newPlayer];
+	}
+
+	const removePlayer = (e) => {
+		players = players.filter(
+			player => player.name !== e.detail)
+	}
 </script>
 
 <Navbar />
 <div class="container">
+	<AddPlayer on:addPlayer={addPlayer}/>
 	{#if players.length === 0}
 		<p>No Players</p>
 	{:else}
 		{#each players as player}
-			<Player name={player.name} points={player.points}/>
+			<Player name={player.name} points={player.points}
+				on:removePlayer={removePlayer}/>
 		{/each}
 	{/if}
 </div>
